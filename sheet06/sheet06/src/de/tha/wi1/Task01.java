56f7482584ab15aab11c321d0d3a274f7a51c52c
Moritz Zimmermann <moritzfcb2105@gmail.com>
2025-11-20 12:10:12 +0100

    sheet 06 import

---
+++ sheet06/sheet06/src/de/tha/wi1/Task01.java
@@ -0,0 +1,16 @@
+package de.tha.wi1;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		
+        // Beispiel-Array mit unregelmäßigen Dimensionen
+        int[][][] data = {
+            { {1, 2, 3}, {4, 5} },
+            { {10}, {20, 30, 40}, {50} },
+            { {7, 8}, {}, {11, 12, 13, 14} }
+        };
+		
+	}
+	
+}
