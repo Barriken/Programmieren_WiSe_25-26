ec5fbdb1b6dc1c65621f566eaf1eb8acd60b9f83
Moritz Zimmermann <moritzfcb2105@gmail.com>
2025-11-20 12:34:20 +0100

    sheet 06 task 01 finished

--- sheet06/sheet06/src/de/tha/wi1/Task01.java
+++ sheet06/sheet06/src/de/tha/wi1/Task01.java
@@ -2,7 +2,8 @@ package de.tha.wi1;
 
 public class Task01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
         // Beispiel-Array mit unregelmäßigen Dimensionen
         int[][][] data = {
@@ -11,6 +12,39 @@ public class Task01 {
             { {7, 8}, {}, {11, 12, 13, 14} }
         };
 		
+        System.out.println("Größte Zahl mit For-Schleife ist " + findMaxForLoop(data));
+        System.out.println("Größte Zahl mit For-Each-Schleife ist " + findMaxForEachLoop(data));
+        
+	}
+	public static int findMaxForLoop(int [][][] a) {
+		int max = Integer.MIN_VALUE;
+    	for (int i = 0; i < a.length; i++) 
+    	{
+    		for (int j = 0; j < a[i].length; j++) 
+    		{
+    			for (int y = 0; y < a[i][j].length; y++) 
+    			{
+    				if (a[i][j][y] > max) 
+    				{
+    					max = a[i][j][y];
+    				}
+    			}
+    		}
+    	}
+    	return max;
+    }
+	public static int findMaxForEachLoop(int [][][] a) {
+		int max = Integer.MIN_VALUE;
+		for (int[][] b : a) {
+			for (int [] c : b) {
+				for (int number : c) {
+					if (number > max) {
+						max = number;
+					}
+				}
+			}
+		}
+		return max;
 	}
 	
 }
