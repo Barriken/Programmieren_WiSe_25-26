5c29d53d56bddc751c7beee8974d609397a776ed
Moritz Zimmermann <moritzfcb2105@gmail.com>
2025-11-21 14:20:13 +0100

    sheet 04 task 03 finished

--- sheet04/src/de/tha/wi1/Task03.java
+++ sheet04/src/de/tha/wi1/Task03.java
@@ -10,10 +10,76 @@ public class Task03 {
 	
 	float[] koefficients = {a, b, c};
 	
-	String outputA = a != 0 ? a + "*x^2" : "";
-	String outputB = b != 0 ? b + "*x" : "";
-	String outputC = c != 0 ? c + "" : "";
-	System.out.println("Formel: " + outputA + outputB + outputC + " = 0");
+	printFormel(a, b, c);
+	printErgebnis(a, b, c);
 	
+	}
+	
+	public static void printErgebnis( float a, float b, float c ) {
+		if (a != 0 && b != 0 && c != 0) {
+			float x1 = (-b + (float)Math.sqrt(b * b - 4 * a *c)) / (2 * a); 
+			float x2 = (-b - (float)Math.sqrt(b * b - 4 * a *c)) / (2 * a); 
+			
+
+			System.out.println("\nDie Gleichung hat zwei reelle Lösungen:");
+			System.out.format(" x1 = %.2f", x1);
+			System.out.format("\n x2 = %.2f", x2);
+		}
+		else if (a == 0 && b != 0) {
+		 float x1 = - (c /b);
+		 System.out.println("\nDie Gleichung hat eine reelle Lösung:");
+		 System.out.format("x = %.2f", x1);
+		}
+		else if ( a == 0 && b == 0 && c == 0) {
+			System.out.println("\nDie Gleichung hat unendlich viele Lösungen.");
+		}
+		else {
+			System.out.println("\nDie Gleichung hat keine reelle Lösung.");
+		}
+	}
+	public static void printFormel( float a, float b, float c) {
+		String outputA = a != 0 ? a + "*x^2" : "";
+		String outputB = b != 0 ? b + "*x" : "";
+		String outputC = c != 0 ? c + "" : "";
+		
+		if (a != 0) {
+			if (a < 0) {
+				outputA = "- " + Math.abs(a) + "*x^2 ";
+			}
+			else {
+				outputA = a + "*x^2 ";
+			}
+		}
+		else {
+			outputA = " ";
+		}
+		
+		if (b != 0) {
+			if (b < 0) {
+				outputB = "- " + Math.abs(b) + "*x ";
+			}
+			else {
+				outputB = "+ " + b + "*x ";
+			}
+		}
+		else {
+			outputB = " ";
+		}
+		
+		if (c != 0) {
+			if (c < 0) {
+				outputC = "- " + Math.abs(c);
+			}
+			else {
+				outputC = "+ " + c + "";
+			}
+		}
+		else {
+			outputC = " ";
+		}
+		
+		
+		System.out.println("\nFormel: " + outputA + outputB + outputC + " = 0");
+		
 	}
 }
