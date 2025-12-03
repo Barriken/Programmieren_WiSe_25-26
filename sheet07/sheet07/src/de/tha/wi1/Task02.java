5607c8790e47b0373ba26872037bb2b4db673508
Moritz Zimmermann <moritzfcb2105@gmail.com>
2025-12-03 15:53:31 +0100

    sheet 07 aufgabe 02 fertig

--- sheet07/sheet07/src/de/tha/wi1/Task02.java
+++ sheet07/sheet07/src/de/tha/wi1/Task02.java
@@ -3,9 +3,16 @@ package de.tha.wi1;
 public class Task02 {
 	public static void main(String[] args) {
 		System.out.println("Größter gemeinsamer Teiler ist: " + ggt(17, 13));
+		System.out.println("Größter gemeinsamer Teiler ist: " + ggt(0, 10));
+		
 	}
 
 	public static int ggt(int x, int y) {
+		
+		// Für Werte == 0 soll 0 returned werden
+		if (x == 0 || y == 0) {
+			return 0;
+		}
 		if (x == y) {
 			return x;
 		}
