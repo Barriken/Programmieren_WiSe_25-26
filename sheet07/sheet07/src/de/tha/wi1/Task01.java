9c77930ca5d8a95c1c5d11019b68938a8fbc42ea
Moritz Zimmermann <moritzfcb2105@gmail.com>
2025-12-09 11:50:23 +0100

    Sheet 07 task 05 angefangen

--- sheet07/sheet07/src/de/tha/wi1/Task01.java
+++ sheet07/sheet07/src/de/tha/wi1/Task01.java
@@ -1,50 +1,27 @@
 package de.tha.wi1;
 
 public class Task01 {
-	public static void main(String[] args) {
-		System.out.println("Ergebnis mit Do While: " + powerDoWhile(2, 0));
-		System.out.println("Ergebnis mit While: " + powerWhile(2, 8));
-		System.out.println("Ergebnis mit Rekursion: " + powerRecursive(2, 8));
-		System.out.println("Ergebnis mit Rekursion (double): " + powerRecursive(2.1, 8));
-	}
 	
-	public static long powerDoWhile (int x, int n) {
-		long result = x;
-		do 
-		{
-			if (n == 0) {
-				return 1;
-			}
-			else if (n > 1) {
-				result = result * x;
-			}
-			n--;
-		} while (n > 1);
-		return result;
-	}
 	
-	public static long powerWhile (int x, int n) {
-		long result = 1;
+	
+	
+	public static long powerDoWhile(int x, int n) {
 		
-		while (n >= 1)
-		{
-			result = result * x;
-			n--;
-		}
-		return result;
+		int i = 1;
+		do {
+			x = x * x;
+			i++;
+		} while ( i < n);
+		
+		return x;
+		
+		
+		
 	}
-	
-	public static long powerRecursive (int x, int n) {
-		if (n == 0) {
-			return 1;
-		}
-		return x * powerRecursive(x,n - 1);
-	}
-	
-	public static double powerRecursive (double x, int n) {
-		if (n == 0) {
-			return 1;
-		}
-		return x * powerRecursive(x,n - 1);
+
+	public static void main(String[] args) {
+		
+		System.out.println(powerDoWhile(3,3));
+		
 	}	
 }
--- sheet07/sheet07/src/de/tha/wi1/Task05.java
+++ sheet07/sheet07/src/de/tha/wi1/Task05.java
@@ -1,5 +1,19 @@
 package de.tha.wi1;
 
 public class Task05 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static char[] convertStringCaesar (String word) {
+		char[] letters = word.toCharArray();
+		
+		for (int i = 0; i < letters.length; i++) {
+			if ((letters[i] < 65 || letters[i] > 90) && (letters[i] < 97 || letters[i] > 122)) {
+				letters[i] = ' ';
+			}
+		}
+		return letters;
+	}
 	
 }
