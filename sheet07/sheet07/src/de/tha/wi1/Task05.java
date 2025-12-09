7bde6a3f6ab4bff585242613365a08dc1235ad3a
Moritz Zimmermann <moritzfcb2105@gmail.com>
2025-12-09 19:07:23 +0100

    Sheet 07 --  Caesars Verschlüsselung fertig

--- sheet07/sheet07/src/de/tha/wi1/Task05.java
+++ sheet07/sheet07/src/de/tha/wi1/Task05.java
@@ -1,8 +1,30 @@
 package de.tha.wi1;
 
 public class Task05 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
+		char[] cleartext = convertStringCaesar("Dies ist ein Test und hier steht ein X");
+		
+		
+		System.out.println("Verschlüsselter Text:" );
+		for (char letter : encrypt(cleartext, (byte)22) ) {
+			System.out.print(letter);
+		}
+		
+		System.out.println();
+		System.out.println();
+		
+		char[] encryptedExample = convertStringCaesar("Bollu xyl Wixy zohencihcyln");
+		
+		System.out.println("Zu entschlüsselnder Text: " + " Bollu xyl Wixy zohencihcyln");
+		System.out.println("Entschlüsselte Form:");
+		for (char letter : encrypt((encryptedExample), (byte)6) ) {
+			System.out.print(letter);
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println("Der Schlüssel war '6'.");
 	}
 	
 	public static char[] convertStringCaesar (String word) {
@@ -14,6 +36,65 @@ public class Task05 {
 			}
 		}
 		return letters;
+	}
+	
+	public static char[] encrypt (char[] clearText, byte shift) {
+		char[] encryptedText = new char[clearText.length];
+		
+		for (int i = 0; i < clearText.length; i++) {
+			encryptedText[i] = encDecChar(clearText[i], shift, true);
+		}
+		return encryptedText;
+	}
+	
+	public static char[] decrypt (char[] encryptedText, byte shift) {
+		char[] clearText = new char[encryptedText.length];
+		
+		for (int i = 0; i < encryptedText.length; i++) {
+			clearText[i] = encDecChar(encryptedText[i], shift, false);
+		}
+		return clearText;
+	}
+	
+	public static char encDecChar (char letter, byte shift, boolean encrypt) 
+	{
+		if (letter == ' ') 
+		{
+			return ' ';
+		}
+		if (letter >= 'A' && letter <= 'Z')
+		{
+			if (encrypt) 
+			{
+				letter = (char)(letter + shift);
+				if (letter > 'Z') {
+					letter = (char)(letter - 26);
+				}
+			}
+			else if (!encrypt) {
+				letter = (char)(letter + shift);
+				if (letter < 'A') {
+					letter = (char)(letter + 26);
+				}
+			}
+		}
+		else if (letter >= 'a' && letter <= 'z')
+		{
+			if (encrypt) 
+			{
+				letter = (char)(letter + shift);
+				if (letter > 'z') {
+					letter = (char)(letter - 26);
+				}
+			}
+			else if (!encrypt) {
+				letter = (char)(letter + shift);
+				if (letter < 'a') {
+					letter = (char)(letter + 26);
+				}
+			}
+		}
+		return letter;
 	}
 	
 }
